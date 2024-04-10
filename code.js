c837e5a0878d275c517a6e04167e3d6d853290be
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-04-10 16:52:44 -0600

    modifying asyncCounter

--- code.js
+++ code.js
@@ -24,3 +24,34 @@ let array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7];
 let key = 3;
 
 console.log(asyncCounter(array, key));
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////code to work on still///////////////
+function asyncCounter(array,key) {
+    let totalCount = array.reduce(work);
+    return totalCount;
+}
+
+function work(count, currentValue, index) {
+    if (currentValue == key) 
+        return count++;   
+    else 
+        return count;
+}
+
+function syncCounter(array, key) {
+    let count = 0;
+    for (let i = 0; i < array.length; i ++) {
+        if (key == array[i])
+            count++;
+    }
+    return count;
+}
+
+
+let array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7];
+let key = 34;
+
+console.log(asyncCounter(array, key));
