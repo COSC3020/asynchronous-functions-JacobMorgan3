eed33d5ba168ed279a0d13ce2354d73c49e94c6f
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-04-12 11:27:55 -0600

    Create code.test.js
    add function that returns true if both inputs are equal. The inputs are the count returned from async and sync counters.
    
    Add tests.

---
+++ code.test.js
@@ -0,0 +1,52 @@
+const code = require("./code.js");
+const assert = require("assert");
+
+fucntion outputEqual (a,b) { //function that returns true if a and b are equal
+  if (a == b)
+    return true;
+  else
+    return false;
+}
+
+let array = [];
+let key = 0;
+
+
+//Test 1    If the key is not in the array it should return 0
+array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7];
+key = 100;
+
+let a = code.asyncCounter(array,key);
+let b = code.syncCounter(array,key);
+
+assert(outputEqual(a,b)); 
+
+
+//Test 2    If the key is in the array 1 time
+array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7];
+key = 34;
+
+a = code.asyncCounter(array,key);
+b = code.syncCounter(array,key);
+
+assert(outputEqual(a,b)); 
+
+
+//Test 3    If the key is in the array x times
+array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7];
+key = 4;
+
+a = code.asyncCounter(array,key);
+b = code.syncCounter(array,key);
+
+assert(outputEqual(a,b)); 
+
+
+//Test 4    If the key is in the array x times
+array = [3,5,7,9,5,21,34,6,8,0,3,1,4,78,0,4,2,1,4,7,78,78,78,78,78,78,78,78];
+key = 78;
+
+a = code.asyncCounter(array,key);
+b = code.syncCounter(array,key);
+
+assert(outputEqual(a,b)); 
